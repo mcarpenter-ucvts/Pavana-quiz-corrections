2b8fab594fce1f25b7505df783a6c24663fbcd60
Marcus <marky0515@gmail.com>
2019-01-19 15:14:17 -0500

    askAndAnswered

---
+++ pavana.js
@@ -0,0 +1,9 @@
+function askedAndAnswered(x, y, z) {
+
+  var x = prompt("Please enter your name. ");
+  var z = prompt("please enter your age");
+  var y = parseInt(z) + 1 ;
+
+  alert("Hello, " + x + ". When do you turn " + y + "?" );
+
+}
